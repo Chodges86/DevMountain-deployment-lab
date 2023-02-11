e872d88d9f83827188bcf71e845dcc7c55e8a252
Caleb Hodges <chodgesdev@gmail.com>
2023-02-11 11:27:19 -0600

    Added rollbar to controller.js file

--- controller.js
+++ controller.js
@@ -1,4 +1,11 @@
 
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '565a491f405b4f978ed6073d0dc3cef3',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
 module.exports = {
 
     nonExistentFunction: () => {
