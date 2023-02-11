656f1799f6f03cb0f0d6c29a34358ad5a78e4445
Caleb Hodges <chodgesdev@gmail.com>
2023-02-11 11:00:56 -0600

    Adding rollbar functionality and setup a fake endpoint to test rollbar.error

--- server.js
+++ server.js
@@ -4,4 +4,23 @@ const app = express()
 
 app.use(express.static(`${__dirname}/public`))
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '565a491f405b4f978ed6073d0dc3cef3',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+app.get("/api/try", () => {
+    try {
+        nonExistentFunction()
+    } catch (err) {
+        rollbar.error("api/try is an endpoint with a non-existent function")
+    }
+})
+
 app.listen(4000, () => console.log("Server up on 4000"))
