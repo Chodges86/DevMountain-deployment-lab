3d9d2f8e7f6216ab1ca4ab40da0c59ba1615edf8
Caleb Hodges <chodgesdev@gmail.com>
2023-02-11 11:56:02 -0600

    Removed rollbar declaration

--- public/main.js
+++ public/main.js
@@ -3,12 +3,6 @@ console.log("Connected")
 const button = document.getElementById('button')
 
 const { default: axios } = require('axios')
-var Rollbar = require('rollbar')
-var rollbar = new Rollbar({
-  accessToken: '565a491f405b4f978ed6073d0dc3cef3',
-  captureUncaught: true,
-  captureUnhandledRejections: true,
-})
 
 function alertMessage() {
     const h1 = document.querySelector('h1')
