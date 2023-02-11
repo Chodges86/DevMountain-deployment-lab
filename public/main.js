4602465cf1bb86d5d19e04b5a56b6318430621bc
Caleb Hodges <chodgesdev@gmail.com>
2023-02-11 12:19:36 -0600

    Removed localhost from api call

--- public/main.js
+++ public/main.js
@@ -5,7 +5,7 @@ const button = document.getElementById('button')
 function alertMessage() {
     const h1 = document.querySelector('h1')
     axios
-    .get("http://localhost:4000/api/message")
+    .get("/api/message")
     .then((res) => h1.textContent = res.data)
     
 }
